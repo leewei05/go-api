eacd7ec0b17c03c449dd6a2fd32a559329775903
leewei05 <lee.wei@ikala.tv>
2020-05-11 00:10:12 +0800

    add pg sql init

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/leewei05/go-api
 
 go 1.13
+
+require github.com/lib/pq v1.5.2
--- main.go
+++ main.go
@@ -1,32 +1,50 @@
 package main
 
 import (
-	"flag"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 var (
-	serverPort = flag.String(
-		"httpPort",
-		os.Getenv("HTTP_PORT"),
-		"Port number of HTTP server",
-	)
+	db *sql.DB
+
+	serverPort = os.Getenv("HTTP_PORT")
+	dbHost     = os.Getenv("PG_HOST")
+	dbPort     = os.Getenv("PG_PORT")
+	dbUser     = os.Getenv("PG_USER")
+	dbPwd      = os.Getenv("PG_PWD")
+	dbName     = os.Getenv("PG_DB")
 )
 
-func init() {
+func initDB() {
+	pgStr := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPwd, dbName)
 
+	if dbHost == "" || dbPort == "" || dbUser == "" || dbPwd == "" || dbName == "" {
+		log.Panicf("Missing config parameters: %v", pgStr)
+	}
+
+	db, err := sql.Open("postgres", pgStr)
+	if err != nil {
+		log.Panic("Cannot open PostgreSQL database")
+	}
+	defer db.Close()
 }
 
 func main() {
-	flag.Parse()
-	if *serverPort == "" {
+	initDB()
+
+	if serverPort == "" {
 		log.Panic("Null HTTP port value")
 	}
 
-	port := fmt.Sprintf(":%v", *serverPort)
+	port := fmt.Sprintf(":%v", serverPort)
 	log.Printf("HTTP server running on port %v", port)
 	http.ListenAndServe(port, nil)
 }
