d20e2047ef06852824e25fc00cb06e6d8e6c0326
leewei05 <lee.wei@ikala.tv>
2020-05-11 20:55:03 +0800

    add config setting

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/leewei05/go-api
 
 go 1.13
 
-require github.com/lib/pq v1.5.2
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/lib/pq v1.5.2
+)
--- main.go
+++ main.go
@@ -7,39 +7,42 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var (
 	db *sql.DB
-
-	serverPort = os.Getenv("HTTP_PORT")
-	dbHost     = os.Getenv("PG_HOST")
-	dbPort     = os.Getenv("PG_PORT")
-	dbUser     = os.Getenv("PG_USER")
-	dbPwd      = os.Getenv("PG_PWD")
-	dbName     = os.Getenv("PG_DB")
 )
 
 func initDB() {
+	_ = godotenv.Load("config.env")
+
+	dbHost := os.Getenv("PG_HOST")
+	dbPort := os.Getenv("PG_PORT")
+	dbUser := os.Getenv("PG_USER")
+	dbPwd := os.Getenv("PG_PWD")
+	dbName := os.Getenv("PG_DB")
+
 	pgStr := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		dbHost, dbPort, dbUser, dbPwd, dbName)
+	fmt.Println(pgStr)
 
 	if dbHost == "" || dbPort == "" || dbUser == "" || dbPwd == "" || dbName == "" {
 		log.Panicf("Missing config parameters: %v", pgStr)
 	}
 
-	db, err := sql.Open("postgres", pgStr)
+	_, err := sql.Open("postgres", pgStr)
 	if err != nil {
 		log.Panic("Cannot open PostgreSQL database")
 	}
-	defer db.Close()
 }
 
 func main() {
 	initDB()
 
+	serverPort := os.Getenv("HTTP_PORT")
 	if serverPort == "" {
 		log.Panic("Null HTTP port value")
 	}
